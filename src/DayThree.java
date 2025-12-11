49f0cf3530b988f2f676b58d02aae31b2ef6cd3b
FahimSyedNY <fahims13@nycstudents.net>
2025-12-11 11:49:46 -0500

    Next Update includes removing all elements before the first max index in the Arraylist, then setting max2 to the new max.

--- src/DayThree.java
+++ src/DayThree.java
@@ -7,19 +7,23 @@ import java.util.Collections;
 
 public class DayThree {
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("Day Three.txt");
+        File f = new File("DayThree.txt");
         Scanner s = new Scanner(f);
-        String[] list;
-        int max = 0;
+        ArrayList<String> list;
+        String max = "";
+        String max2 = "";
+        int index;
         while (s.hasNextLine()) {
-            max = 0;
-            list = s.nextLine().split("");
-            for (int i = 0; i < list.length; i++) {
-                if (Integer.parseInt(list[i]) > max) {
-                    max = Integer.parseInt(list[i]); // Update max if a larger element is found
-                }
+            list = new ArrayList<>(Arrays.asList(s.nextLine().split("")));
+            max = Collections.max(list);
+            index = list.indexOf(max);
+            list.set(index, "0");
+            max2 = Collections.max(list);
+            if (list.indexOf(max2) > index) System.out.println(max + max2);
+            if (list.indexOf(max2) < index) {
+                if (Integer.parseInt(max2) > Integer.parseInt(max)) System.out.println(max2 + max);
+                // Use list.subList(1, 4).clear()
             }
-            System.out.println(max);
         }
     }
 }
