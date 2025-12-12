ba04467e8491c98d6326ae03af9e1995d9b40343
FahimSyedNY <fahims13@nycstudents.net>
2025-12-12 00:48:56 -0500

    Removed redundant method

--- src/DayThree.java
+++ src/DayThree.java
@@ -7,9 +7,10 @@ import java.util.Collections;
 
 public class DayThree {
     public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(anyDigit(2));
         System.out.println(anyDigit(12));
-        System.out.println(twoDigit());
     }
+
     public static long anyDigit(int length) throws FileNotFoundException {
         File f = new File("DayThree.txt");
         Scanner s = new Scanner(f);
@@ -26,7 +27,7 @@ public class DayThree {
             size = templist.size();
             voltage = "";
             int i = length;
-            while(voltage.length() < length) {
+            while (voltage.length() < length) {
                 max = Collections.max(templist);
                 index = templist.indexOf(max);
                 if (index <= size - i) {
@@ -34,35 +35,9 @@ public class DayThree {
                     templist = new ArrayList<>(list);
                     Collections.fill(templist.subList(0, index + 1), "0");
                     i--;
-                }
-                else templist.set(index, "0");
+                } else templist.set(index, "0");
             }
             sum += Long.parseLong(voltage);
-        }
-        return sum;
-    }
-    public static int twoDigit() throws FileNotFoundException {
-        File f = new File("DayThree.txt");
-        Scanner s = new Scanner(f);
-        ArrayList<String> list;
-        String max;
-        String max2;
-        int index;
-        int sum = 0;
-        while (s.hasNextLine()) {
-            list = new ArrayList<>(Arrays.asList(s.nextLine().split("")));
-            max = Collections.max(list);
-            index = list.indexOf(max);
-            list.set(index, "0");
-            max2 = Collections.max(list);
-            if (list.indexOf(max2) > index) sum += Integer.parseInt(max + max2);
-            else {
-                if (list.size() == index + 1) sum += Integer.parseInt(max2 + max);
-                else {
-                    list.subList(0, index).clear();
-                    sum += Integer.parseInt(max + Collections.max(list));
-                }
-            }
         }
         return sum;
     }
