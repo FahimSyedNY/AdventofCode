7b424fe9c6cd0fe6f2b59650c15fe81324465f60
FahimSyedNY <fahims13@nycstudents.net>
2025-12-11 23:01:09 -0500

    Finished Day three, implemented case with length check rather than comparing max.

--- src/DayThree.java
+++ src/DayThree.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.Collections;
 
 public class DayThree {
@@ -10,20 +10,25 @@ public class DayThree {
         File f = new File("DayThree.txt");
         Scanner s = new Scanner(f);
         ArrayList<String> list;
-        String max = "";
-        String max2 = "";
+        String max;
+        String max2;
         int index;
+        int sum = 0;
         while (s.hasNextLine()) {
             list = new ArrayList<>(Arrays.asList(s.nextLine().split("")));
             max = Collections.max(list);
             index = list.indexOf(max);
             list.set(index, "0");
             max2 = Collections.max(list);
-            if (list.indexOf(max2) > index) System.out.println(max + max2);
-            if (list.indexOf(max2) < index) {
-                if (Integer.parseInt(max2) > Integer.parseInt(max)) System.out.println(max2 + max);
-                // Use list.subList(1, 4).clear()
+            if (list.indexOf(max2) > index) sum += Integer.parseInt(max + max2);
+            else {
+                if (list.size() == index + 1) sum += Integer.parseInt(max2 + max);
+                else {
+                    list.subList(0, index).clear();
+                    sum += Integer.parseInt(max + Collections.max(list));
+                }
             }
         }
+        System.out.println(sum);
     }
 }
