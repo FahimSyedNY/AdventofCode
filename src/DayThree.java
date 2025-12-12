894e5b25e4bc3a5c24f2752fefd3adfa6250fb69
FahimSyedNY <fahims13@nycstudents.net>
2025-12-12 00:41:04 -0500

    Added Part 2 of Day 3, new method works with any length for the size of the voltage (so long as the "long" data type can hold it).

--- src/DayThree.java
+++ src/DayThree.java
@@ -7,6 +7,41 @@ import java.util.Collections;
 
 public class DayThree {
     public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(anyDigit(12));
+        System.out.println(twoDigit());
+    }
+    public static long anyDigit(int length) throws FileNotFoundException {
+        File f = new File("DayThree.txt");
+        Scanner s = new Scanner(f);
+        ArrayList<String> list;
+        ArrayList<String> templist;
+        int index;
+        int size;
+        long sum = 0;
+        String voltage;
+        String max;
+        while (s.hasNextLine()) {
+            list = new ArrayList<>(Arrays.asList(s.nextLine().split("")));
+            templist = new ArrayList<>(list);
+            size = templist.size();
+            voltage = "";
+            int i = length;
+            while(voltage.length() < length) {
+                max = Collections.max(templist);
+                index = templist.indexOf(max);
+                if (index <= size - i) {
+                    voltage += max;
+                    templist = new ArrayList<>(list);
+                    Collections.fill(templist.subList(0, index + 1), "0");
+                    i--;
+                }
+                else templist.set(index, "0");
+            }
+            sum += Long.parseLong(voltage);
+        }
+        return sum;
+    }
+    public static int twoDigit() throws FileNotFoundException {
         File f = new File("DayThree.txt");
         Scanner s = new Scanner(f);
         ArrayList<String> list;
@@ -29,6 +64,6 @@ public class DayThree {
                 }
             }
         }
-        System.out.println(sum);
+        return sum;
     }
 }
