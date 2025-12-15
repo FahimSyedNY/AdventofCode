99578dfe08c8db1b5806f929e988a1c95c98e6c1
FahimSyedNY <fahims13@nycstudents.net>
2025-12-14 20:31:05 -0500

    DayTwo Part 2

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -7,18 +7,42 @@ public class DayTwo {
         File f = new File("DayTwo.txt");
         Scanner s = new Scanner(f);
         String[] list = s.nextLine().split(",");
+        int[] factors;
+        int pos;
         int dashInd;
         int size;
         long sum = 0;
         String num;
-        for (String string : list) {
-            dashInd = string.indexOf('-');
-            for (long j = Long.parseLong(string.substring(0, dashInd)); j <= Long.parseLong(string.substring(dashInd + 1)); j++) {
+        boolean isInvalid;
+        for (String str : list) {
+            dashInd = str.indexOf('-');
+            for (long j = Long.parseLong(str.substring(0, dashInd)); j <= Long.parseLong(str.substring(dashInd + 1)); j++) {
                 num = j + "";
                 size = num.length();
-                if (size % 2 == 0 && num.substring(0, size / 2).equals(num.substring(size / 2))) sum += j;
+                factors = factors(size);
+                isInvalid = false;
+                for (int i = factors.length - 1; i >= 0; i--) {
+                    pos = factors[i];
+                    while (pos != size) {
+                        if (!(num.substring(pos - factors[i], pos).equals(num.substring(pos, pos + factors[i])))) break;
+                        else if (pos + factors[i] == size) {
+                            sum += j;
+                            isInvalid = true;
+                        }
+                        pos += factors[i];
+                    }
+                    if (isInvalid) break;
+                }
             }
         }
         System.out.println(sum);
+    }
+    public static int[] factors(int n) {
+        String factors = "1,";
+        for (int i = 2; i <= n / 2; i++) if (n % i == 0) factors += i + ",";
+        String[] stringArray = factors.split(",");
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) intArray[i] = Integer.parseInt(stringArray[i]);
+        return intArray;
     }
 }
