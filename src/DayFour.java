411b0cd87bd239b57736b490e99d3806118b2b61
FahimSyed <fahims13@nycstudents.net>
2025-12-16 10:53:16 -0500

    Finished Part 1 & 2 of DayFour

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -7,26 +8,36 @@ public class DayFour {
     public static void main(String[] args) throws FileNotFoundException {
         File f = new File("DayFour.txt");
         Scanner s = new Scanner(f);
-        ArrayList<String[]> list = new ArrayList<>();
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
         int[][] addList = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         int count;
-        int canMove = 0;
+        int canMove = 1;
+        int sum = 0;
         while (s.hasNextLine()) {
-            list.add(s.nextLine().split(""));
+            list.add(new ArrayList<>(Arrays.asList(s.nextLine().split(""))));
         }
-        for (int k = 0; k < list.size(); k++) {
-            for (int j = 0; j < list.get(k).length; j++) {
-                count = 0;
-                if (!(list.get(k)[j].equals("@"))) break;
-                for (int i = 0; i < 8 && count < 4; i++) {
-                    if (k + addList[i][0] > -1 && j + addList[i][1] > -1 && k + addList[i][0] < list.size() && j + addList[i][1] < list.get(k).length) {
-                        if (list.get(k + addList[i][0])[j + addList[i][1]].equals("@")) count++;
+        int size = list.size();
+        int length = list.getFirst().size();
+        while (canMove > 0) {  // 1. Remove while loop-
+            canMove = 0;
+            for (int k = 0; k < size; k++) {
+                for (int j = 0; j < length; j++) {
+                    if (list.get(k).get(j).equals("@")) {
+                        count = 0;
+                        for (int i = 0; i < 8 && count < 4; i++) {
+                            if (k + addList[i][0] > -1 && j + addList[i][1] > -1 && k + addList[i][0] < size && j + addList[i][1] < length) {
+                                if (list.get(k + addList[i][0]).get(j + addList[i][1]).equals("@")) count++;
+                            }
+                        }
+                        if (count < 4) {
+                            canMove++;
+                            list.get(k).set(j, "."); // 2. Remove set at index
+                        }
                     }
                 }
-                System.out.println("" + k + j + ": " + count);
-                if (count < 4) canMove++;
             }
+            sum += canMove; // 3. and report canMove instead of sum to find Part 1
         }
-        System.out.println(canMove);
+        System.out.println(sum);
     }
 }
