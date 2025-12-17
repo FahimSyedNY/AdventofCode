1119954dfae289ff310d81d49f2e20fd48c3daf7
FahimSyed <fahims13@nycstudents.net>
2025-12-17 11:02:38 -0500

    Started Day 5 part 1

---
+++ src/DayFive.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DayFive {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("DayFive.txt");
+        Scanner s = new Scanner(f);
+        ArrayList<String[]> list = new ArrayList<>();
+        String[] nextLine = null;
+        while (nextLine.length != 0) {
+            list.add(nextLine);
+            nextLine = s.nextLine().split("-");
+        }
+    }
+}
