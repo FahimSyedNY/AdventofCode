78393cedd868bcceaae2d3faf4994670c9f6231a
FahimSyedNY <fahims13@nycstudents.net>
2025-12-26 00:13:20 -0500

    Finished Day8

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-//        System.out.println("Day 1:");
-//        Day1_SecretEntrance.run();
-//        System.out.println("\nDay 2:");
-//        Day2_GiftShop.run();
-//        System.out.println("\nDay 3:");
-//        Day3_Lobby.run();
-//        System.out.println("\nDay 4:");
-//        Day4_PrintingDepartment.run();
-//        System.out.println("\nDay 5:");
-//        Day5_Cafeteria.run();
-//        System.out.println("\nDay 6:");
-//        Day6_TrashCompactor.run();
-//        System.out.println("\nDay 7:");
-//        Day7_Laboratories.run();
+        System.out.println("Day 1:");
+        Day1_SecretEntrance.run();
+        System.out.println("\nDay 2:");
+        Day2_GiftShop.run();
+        System.out.println("\nDay 3:");
+        Day3_Lobby.run();
+        System.out.println("\nDay 4:");
+        Day4_PrintingDepartment.run();
+        System.out.println("\nDay 5:");
+        Day5_Cafeteria.run();
+        System.out.println("\nDay 6:");
+        Day6_TrashCompactor.run();
+        System.out.println("\nDay 7:");
+        Day7_Laboratories.run();
         System.out.println("\nDay 8:");
         Day8_Playground.run();
     }
