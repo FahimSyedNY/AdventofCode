6d7a02d1ee182634d522921bf7ce3d9eda062521
FahimSyed <fahims13@nycstudents.net>
2026-01-08 11:05:01 -0500

    Added board viewer for coords.

--- src/Day9_MovieTheater.java
+++ src/Day9_MovieTheater.java
@@ -6,32 +6,34 @@ public class Day9_MovieTheater {
     public static void run() throws FileNotFoundException {
         File f = new File("Day9_MovieTheater.txt");
         Scanner s = new Scanner(f).useDelimiter(",|\\r?\\n");
-        ArrayList<int[]> coords = new ArrayList<>();
+        ArrayList<Integer> xcoords = new ArrayList<>();
+        ArrayList<Integer> ycoords = new ArrayList<>();
         ArrayList<Long> areas = new ArrayList<>();
-        while (s.hasNextInt()) coords.add(new int[] {s.nextInt(), s.nextInt()});
-
+        while (s.hasNextInt()) {
+            xcoords.add(s.nextInt());
+            ycoords.add(s.nextInt());
+        }
         // Part One:
-        int size = coords.size();
-        int[] point1;
-        int[] point2;
+        int size = xcoords.size();
         for (int i = 0; i < size - 1; i++) for (int j = i + 1; j < size; j++) {
-            point1 = coords.get(i);
-            point2 = coords.get(j);
-            areas.add(((long) point1[0] - point2[0] + 1) * ((point1[1] - point2[1] + 1)));
+            areas.add(((long) xcoords.get(i) - xcoords.get(j) + 1) * ((ycoords.get(i) - ycoords.get(j) + 1)));
         }
         System.out.println(Collections.max(areas));
         // Part Two:
-
-        for (int i = 0; i < Collections.max(coords, Comparator.comparingInt(a -> a[1]))[1]; i++) {
-            for (int j = 0; j < Collections.max(coords, Comparator.comparingInt(a -> a[0]))[0]; j++) {
-                if (containsIntLst(coords, j, i)) System.out.print("#");
+        for (int i = 0; i < Collections.max(ycoords) + 1; i++) {
+            for (int j = 0; j < Collections.max(xcoords) + 1; j++) {
+                if (containslist(xcoords, ycoords, j, i)) System.out.print("#");
                 else System.out.print(".");
             }
             System.out.println();
         }
     }
-    public static boolean containsIntLst(ArrayList<int[]> arys, int x, int y) {
-        for (int[] ary : arys) if (Arrays.equals(ary, new int[] {x, y})) return true;
+    public static boolean containslist(ArrayList<Integer> origxs, ArrayList<Integer> ys, int x, int y) {
+        ArrayList<Integer> xs = new ArrayList<Integer>(origxs);
+        while (xs.contains(x)) {
+            if (ys.get(xs.indexOf(x)).equals(y)) return true;
+            else xs.set(xs.indexOf(x), -1);
+        }
         return false;
     }
 }
